d7582089e903015922e1f9e34076e87cd70fe899
daniel <b04902002@ntu.edu.tw>
2018-06-04 20:47:01 +0800

    update data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -13,7 +13,7 @@ DATA_DIR = "../data/"
 def load_yuv(path):
     with open(path, "rb") as fin:
         data = fin.read()
-    
+
     frame_num = len(data) // frame_data_size
     
     data = bytearray(data)
@@ -23,7 +23,7 @@ def load_yuv(path):
     Y = data[:, :frame_size].reshape([frame_num, video_shape[0], video_shape[1]])
     Cb = data[:, frame_size:frame_size*5//4].reshape([frame_num, subsample_shape[0], subsample_shape[1]]).repeat(4, axis=2)
     Cr = data[:, frame_size*5//4:frame_size*3//2].reshape([frame_num, subsample_shape[0], subsample_shape[1]]).repeat(4, axis=2)
-    
+
     yuv_array = np.zeros([frame_num, *video_shape, 3])
     yuv_array[:, :, :, 0] = Y
     yuv_array[:, :, :, 1] = Cb
@@ -32,8 +32,8 @@ def load_yuv(path):
     return yuv_array
 
 
-def load_all_yuv():
-    yuv_path_list = sorted(glob.glob(os.path.join(DATA_DIR, "*.yuv")))
+def load_all_yuv(data_dir):
+    yuv_path_list = sorted(glob.glob(os.path.join(data_dir, "*.yuv")))
     yuv_data_list = []
     for yuv_path in yuv_path_list:
         yuv_data_list.append(load_yuv(yuv_path))
