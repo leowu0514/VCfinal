fc6ed54c52667d9583f5de86693acd093456112b
daniel <b04902002@ntu.edu.tw>
2018-06-08 20:01:39 +0800

    update data_processing with yuv2rgb & modify the loading process

--- src/data_processing.py
+++ src/data_processing.py
@@ -5,7 +5,7 @@ import os
 video_shape = (144, 176)
 frame_size = video_shape[0] * video_shape[1]
 frame_data_size = int(frame_size * 1.5)
-subsample_shape = (video_shape[0], video_shape[1]//4)
+subsample_shape = (video_shape[0]//2, video_shape[1]//2)
 
 DATA_DIR = "../data/"
 
@@ -21,14 +21,14 @@ def load_yuv(path):
     data = data.reshape([frame_num, -1])
     
     Y = data[:, :frame_size].reshape([frame_num, video_shape[0], video_shape[1]])
-    Cb = data[:, frame_size:frame_size*5//4].reshape([frame_num, subsample_shape[0], subsample_shape[1]]).repeat(4, axis=2)
-    Cr = data[:, frame_size*5//4:frame_size*3//2].reshape([frame_num, subsample_shape[0], subsample_shape[1]]).repeat(4, axis=2)
+    Cb = data[:, frame_size:frame_size*5//4].reshape([frame_num, subsample_shape[0], subsample_shape[1]]).repeat(2, axis=2).repeat(2, axis=1)
+    Cr = data[:, frame_size*5//4:frame_size*3//2].reshape([frame_num, subsample_shape[0], subsample_shape[1]]).repeat(2, axis=2).repeat(2, axis=1)
 
     yuv_array = np.zeros([frame_num, *video_shape, 3])
     yuv_array[:, :, :, 0] = Y
     yuv_array[:, :, :, 1] = Cb
     yuv_array[:, :, :, 2] = Cr
-    
+
     return yuv_array
 
 
@@ -40,4 +40,13 @@ def load_all_yuv(data_dir):
 
     return yuv_data_list
 
+
+
+def yuv_to_rgb(yuv):
+    rgb = np.zeros_like(yuv)
+    rgb[:, :, :, 0] = yuv[:, :, :, 0] + 1.402 * (yuv[:, :, :, 2] - 128)
+    rgb[:, :, :, 1] = yuv[:, :, :, 0] - 0.344 * (yuv[:, :, :, 1] - 128) - 0.714 * (yuv[:, :, :, 2] - 128)
+    rgb[:, :, :, 2] = yuv[:, :, :, 0] + 1.772 * (yuv[:, :, :, 1] - 128)
+                                                 
+    return np.clip(np.round(rgb), 0, 255).astype(np.uint8)
 
